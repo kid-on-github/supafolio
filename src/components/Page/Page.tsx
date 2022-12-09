f59d6e3f11b3c9b9020fce9b5add0b54fc2df4e8
Mike <mike@lemonshell.com>
2022-12-09 01:02:09 -0700

    update page

--- src/components/Page/Page.tsx
+++ src/components/Page/Page.tsx
@@ -4,11 +4,5 @@ import styles from './Page.module.css'
 export const Page: FunctionComponent<{
 	children: JSX.Element | JSX.Element[]
 }> = ({ children }) => {
-	return (
-		<div className={styles.Page}>
-			<div className={styles.PageContentWrapper}>
-				<div className={styles.PageContent}>{children}</div>
-			</div>
-		</div>
-	)
+	return <div className={styles.Page}>{children}</div>
 }
