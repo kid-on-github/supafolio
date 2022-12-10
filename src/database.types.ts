b80d1fcc2555c8c2faa160e0755bb3d3ea295126
Mike <mike@lemonshell.com>
2022-12-10 02:17:59 -0700

    update types for new db schema

--- src/database.types.ts
+++ src/database.types.ts
@@ -13,14 +13,38 @@ export interface Database {
         Row: {
           user_id: string
           full_name: string
+          email: string
+          cell: string
+          company: string
+          company_website: string
+          linkedin: string
+          instagram: string
+          facebook: string
+          twitter: string
         }
         Insert: {
           user_id: string
           full_name: string
+          email: string
+          cell: string
+          company: string
+          company_website: string
+          linkedin: string
+          instagram: string
+          facebook: string
+          twitter: string
         }
         Update: {
           user_id?: string
           full_name?: string
+          email?: string
+          cell?: string
+          company?: string
+          company_website?: string
+          linkedin?: string
+          instagram?: string
+          facebook?: string
+          twitter?: string
         }
       }
     }
