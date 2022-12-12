e12c0066343a9ddd055dc9668be2e60f1c4d8684
Mike <mike@lemonshell.com>
2022-12-11 22:32:34 -0700

    temp auth routing solution

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,8 +1,19 @@
+import { useContext } from 'react'
+import { Navigate } from 'react-router-dom'
+import { UserContext } from '../../App'
 import MagicLinkForm from '../MagicLinkForm/MagicLinkForm'
 import { Page } from '../Page/Page'
 import styles from './Home.module.css'
 
 const Home = () => {
+	// TODO: figure out a better auth redirect solution
+	const user = useContext(UserContext)
+	const { id } = user.session?.user ?? {}
+
+	if (id) {
+		return <Navigate to='/profile' />
+	}
+
 	return (
 		<Page>
 			<div className={styles.Home}>
--- src/components/Profile/ProfileEdit/ProfileEdit.tsx
+++ src/components/Profile/ProfileEdit/ProfileEdit.tsx
@@ -1,4 +1,5 @@
-import { useContext } from 'react'
+import { useContext, useState } from 'react'
+import { Navigate } from 'react-router-dom'
 import { UserContext } from '../../../App'
 import { supaClient } from '../../../utils/supaClient'
 import styles from '../Profile.module.css'
@@ -17,16 +18,29 @@ const ProfileEdit = () => {
 
 	const previewLink = `${baseURL}/profile/${id}`
 
-	return id ? (
-		<div className={styles.FlexSpacing}>
-			<QrCode url={previewLink} />
-			<ProfileForm />
-			<hr />
-			<button className={styles.LogOut} onClick={() => signOut()}>
-				Log Out
-			</button>
-		</div>
-	) : null
+	// TODO: figure out a better auth redirect solution
+	const [redirect, setRedirect] = useState(false)
+	supaClient.auth.getSession().then(({ data: { session } }) => {
+		console.log('xxx', session?.user?.id)
+		if (!session?.user?.id) {
+			setRedirect(true)
+		}
+	})
+
+	if (redirect) {
+		return <Navigate to='/' />
+	} else {
+		return id ? (
+			<div className={styles.FlexSpacing}>
+				<QrCode url={previewLink} />
+				<ProfileForm />
+				<hr />
+				<button className={styles.LogOut} onClick={() => signOut()}>
+					Log Out
+				</button>
+			</div>
+		) : null
+	}
 }
 
 export default ProfileEdit
