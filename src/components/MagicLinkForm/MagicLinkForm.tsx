ad832aa248f618b035114b3805623e98eb915dff
Mike <mike@lemonshell.com>
2022-12-13 23:19:23 -0700

    add magic link message

--- src/components/MagicLinkForm/MagicLinkForm.tsx
+++ src/components/MagicLinkForm/MagicLinkForm.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { supaClient } from '../../utils/supaClient'
 import styles from './MagicLinkForm.module.css'
 
@@ -6,7 +6,6 @@ async function signInWithEmail(email: string) {
 	const { data, error } = await supaClient.auth.signInWithOtp({
 		email,
 	})
-
 	console.log('data', data)
 	console.log('error', error)
 	// TODO: handle error
@@ -14,18 +13,36 @@ async function signInWithEmail(email: string) {
 
 const MagicLinkForm = () => {
 	const [email, setEmail] = useState('')
+	const [clicked, setClicked] = useState(false)
+
+	const updateClicked = () => setClicked(true)
+
+	useEffect(() => {
+		if (clicked) {
+			setTimeout(() => {
+				setClicked(false)
+			}, 10000)
+		}
+	}, [clicked])
 
 	return (
-		<div className={styles.MagicLinkForm}>
-			<input
-				type='email'
-				placeholder='John@example.com'
-				onChange={({ target: { value } }) => setEmail(value)}
-			/>
-			<button onClick={() => signInWithEmail(email)}>
-				<span className='material-symbols-outlined'>send</span>
-			</button>
-		</div>
+		<>
+			<div className={styles.MagicLinkForm}>
+				<input
+					type='email'
+					placeholder='John@example.com'
+					onChange={({ target: { value } }) => setEmail(value)}
+				/>
+				<button onClick={() => signInWithEmail(email).then(updateClicked)}>
+					<span className='material-symbols-outlined'>send</span>
+				</button>
+			</div>
+			{clicked && (
+				<div className={styles.EmailSentMsg}>
+					<h4>Click the link in your email to log in!</h4>
+				</div>
+			)}
+		</>
 	)
 }
 
